abaea22c29c0a969d5c7fa73d754bd7bb7fdbd1c
Henri Bergius <henri.bergius@iki.fi>
2011-12-01 13:29:41 +0200

    More detailed assertion messages

--- test/zombie-qunit.js
+++ test/zombie-qunit.js
@@ -2,13 +2,14 @@ var zombie = require("zombie");
 var express = require("express");
 var _ = require("underscore")._;
 
-exports['test QUnit with a headless browser'] = function(test) {
+var qunitResults;
+
+exports.setUp = function(callback) {
     var server = express.createServer();
     server.configure(function() {
         server.use(express.errorHandler());
         server.use("/", express.static(__dirname + "/../"));
         server.use("/", express.directory(__dirname + "/../"));
-        server.use(express.logger());
     });
     server.listen(3000, function() {
         var location = "http://localhost:3000/qunit/index.html";
@@ -18,15 +19,22 @@ exports['test QUnit with a headless browser'] = function(test) {
             browser.fire('load', browser.window);
 
             browser.wait(function(err, browser) {
-                _.each(browser.css('#qunit-tests > li'), function(listItem) {
-                    var testMessage = listItem._childNodes[0]._childNodes[0].textContent + ': ' + listItem._childNodes[0]._childNodes[2].textContent;
-                    var testState = listItem._attributes._nodes['class']._nodeValue;
-
-                    test.equal(testState, 'pass', testMessage);
-                });
+                qunitResults = browser.css('#qunit-tests > li');
                 server.close();
-                test.done();
+                callback();
             });
         });
     });
+};
+
+exports['test QUnit with a headless browser'] = function(test) {
+    _.each(qunitResults, function(listItem) {
+        var testGroup = listItem._childNodes[0]._childNodes[0].textContent + ': ' + listItem._childNodes[0]._childNodes[2].textContent;
+        _.each(listItem._childNodes[2]._childNodes, function(individualTest) {
+            var testMessage = individualTest.textContent;
+            var testState = individualTest._attributes._nodes['class']._nodeValue;
+            test.equal(testState, 'pass', testGroup + ': ' + testMessage);
+        });
+    });
+    test.done();
 };
